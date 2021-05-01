d53ddce64d65d960663caac9e4ee71ccc1d437c8
Akshay <narulaakshay01@gmail.com>
2021-05-01 13:09:09 -0500

    mock test repo

--- src/main/java/com/example/TesterApp/models/TestModel.java
+++ src/main/java/com/example/TesterApp/models/TestModel.java
@@ -1,24 +1,10 @@
 package com.example.TesterApp.models;
 
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document
 public class TestModel{
 
-	@Id
-	ObjectId id;
-	
+
 	String name;
 
-	public ObjectId getId() {
-		return id;
-	}
-
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
